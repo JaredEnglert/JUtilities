85f9428f27a1ed67619ee7263bb6759f6c95fd68
Jared Englert <Jared.Englert@gmail.com>
2016-09-14 23:58:47 -0400

    Added Feature Toggles

---
+++ src/Utilitarian.FeatureToggles.Test.Unit/TestClasses/FeatureToggleServiceTests.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Utilitarian.FeatureToggles.Test.Unit.TestClasses
+{
+    [TestClass]
+    public class FeatureToggleServiceTests
+    {
+        private readonly IFeatureToggleService _featureToggleService;
+
+        public FeatureToggleServiceTests()
+        {
+            _featureToggleService = new FeatureToggleService(new List<IFeatureToggle> {
+                new TrueFeature(),
+                new FalseFeature(),
+                new DynamicFeature()
+            });
+        }
+
+        [TestMethod]
+        public void ShouldReturnTrueWhenFeatureIsOn()
+        {
+            _featureToggleService.IsEnabled("TrueFeature").ShouldBeEquivalentTo(true);
+        }
+
+        [TestMethod]
+        public void ShouldReturnFalseWhenFeatureIsOff()
+        {
+            _featureToggleService.IsEnabled("FalseFeature").ShouldBeEquivalentTo(false);
+        }
+
+        [TestMethod]
+        public void ShouldReturnFalseWhenFeatureDoesNotExist()
+        {
+            _featureToggleService.IsEnabled("DoesNotExist").ShouldBeEquivalentTo(false);
+        }
+
+        [TestMethod]
+        public void ShouldReturnFalseWhenNameIsNull()
+        {
+            _featureToggleService.IsEnabled(null).ShouldBeEquivalentTo(false);
+        }
+
+        [TestMethod]
+        public void ShouldReturnCorrectDynamicValue()
+        {
+            _featureToggleService.IsEnabled("DynamicFeature", true).ShouldBeEquivalentTo(false);
+        }
+
+        private class TrueFeature : IFeatureToggle
+        {
+            public string Name => "TrueFeature";
+
+            public bool IsEnabled(object @object = null)
+            {
+                return true;
+            }
+        }
+
+        private class FalseFeature : IFeatureToggle
+        {
+            public string Name => "FalseFeature";
+
+            public bool IsEnabled(object @object = null)
+            {
+                return false;
+            }
+        }
+
+        private class DynamicFeature : IFeatureToggle
+        {
+            public string Name => "DynamicFeature";
+
+            public bool IsEnabled(object @object = null)
+            {
+                return !(bool)@object;
+            }
+        }
+    }
+}
---
+++ src/Utilitarian.FeatureToggles/FeatureToggleService.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Utilitarian.FeatureToggles
+{
+    public interface IFeatureToggleService
+    {
+        bool IsEnabled(string featureToggleName, object @object = null);
+    }
+
+    public class FeatureToggleService : IFeatureToggleService
+    {
+        private readonly ConcurrentDictionary<string, IFeatureToggle> _featureToggles;
+
+        public FeatureToggleService(IEnumerable<IFeatureToggle> featureToggles)
+        {
+            _featureToggles = new ConcurrentDictionary<string, IFeatureToggle>(featureToggles.ToDictionary(featureToggle => featureToggle.Name));
+        }
+
+        public bool IsEnabled(string featureToggleName, object @object = null)
+        {
+            return !string.IsNullOrWhiteSpace(featureToggleName) && _featureToggles.ContainsKey(featureToggleName) 
+                ? _featureToggles[featureToggleName].IsEnabled(@object)
+                : false;
+        }
+    }
+}
