44b1690f922bbfead3385fa9eed331604759d342
Jared Englert <Jared.Englert@gmail.com>
2017-03-19 00:07:36 -0400

    Added Remove extensions to collections

---
+++ src/Utilitarian.Extensions/CollectionExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Utilitarian.Extensions
+{
+    public static class CollectionExtensions
+    {
+        public static T RemoveItem<T>(this ICollection<T> collection, Func<T, bool> predicate)
+        {
+            var item = collection.FirstOrDefault(predicate);
+
+            if (item != null) collection.Remove(item);
+
+            return item;
+        }
+
+        public static ICollection<T> RemoveAll<T>(this ICollection<T> collection, Func<T, bool> predicate)
+        {
+            foreach (var item in collection.Where(predicate).ToList())
+            {
+                collection.Remove(item);
+            }
+
+            return collection;
+        }
+    }
+}
